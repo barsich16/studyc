81b3b89900b7a5fe50c552c5e53630eb366c98c8
name <bohdanborisenko8@gmail.com>
2022-07-23 14:56:10 +0300

    dep6

--- client/src/redux/api-config.js
+++ client/src/redux/api-config.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const $authHost = axios.create({
-    baseURL: 'http://localhost:3000'
+    // baseURL: 'http://localhost:3000'
+    baseURL: 'https://studyc.herokuapp.com'
 });
 const authInterceptor = config => {
     config.headers.authorization = `Bearer ${JSON.parse(localStorage.getItem('userData')).token}`;
